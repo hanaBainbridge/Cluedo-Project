0b4ee6b7f19ac3262059e3b5bbe0cfbf5efcc142
Connor <connora.wellington18@gmail.com>
2016-07-24 12:28:56 +1200

    Update Board.java

--- Cluedo/src/game_elements/Board.java
+++ Cluedo/src/game_elements/Board.java
@@ -4,7 +4,7 @@ public class Board {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		// f### git its hard
 		// ok so today i learned that git is hard 
 
 	}
